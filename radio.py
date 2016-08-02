e91d3ae073320beea589b09bcfd0c38383519552
adrian123454321 <adrian.lora12345@gmail.com>
2016-08-01 20:32:45 -0600

    ah

--- radio.py
+++ radio.py
@@ -1,8 +1,9 @@
-class radio():
+class Radio():
 	def __init__(self,marca):
 		self.marca=marca
 		self.encendido=False
 		self.en_Fm=True
+		self.en_am=False
 		self.emisora=88.0
 		self.volumen=0
 
@@ -19,18 +20,48 @@ class radio():
 			self.volumen+=5
 
 	def bajar_volumen(self):
-		if self.vomlumen<=0
+		if self.vomlumen<=0:
 			self.volumen=0
 		else:
 			self.volumen-=5
 
 	def subir_emisora(self):
-		if 	self.en_Fm>107.0:
-			self.en_Fm=87.0
-		else:
-			sel.em_Fm+=0.5
+		if not en_fm:
+		 self.en_am=True
+		 if self.en_am>=1300:
+		 	self.en_am=300
+		 else:
+		 	self.en_am+=40
 
-	def bajar_emisora
+	def bajar_emisora(self):
+		if not en_fm:
+			self.en_am=True
+			if self.en_am<=300:
+				self.en_am=1300
+			else:
+				self.en_am-=40
+
+	def subir_emisora(self):
+		if not en_am:
+			self.en_fm=True
+			if self.en_fm<=107.0:
+				self.en_fm=88.0
+			else:
+				self.en_fm+=0.5
+
+	def bajar_emisora(self):
+		if not en_am:
+			self.en_fm=True
+			if sefl.en_fm<=88.0:
+				self.en_fm=107.0
+			else:
+
+
+
+
+
+
+			 	
 
 
 
